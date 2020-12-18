5356f440a35a7b1bb0deccb163e755beed2e8050
Ariel <ariellunen@gmail.com>
2020-12-18 12:52:49 +0200

    user form

--- Client/js/functions.js
+++ Client/js/functions.js
@@ -1,1 +1,27 @@
-// Change the name of the file to something less generic
+
+
+function addUser(info) {
+    $.ajax({
+        url: `http://localhost:3000/api/restaurants`,
+        type: 'POST',
+        data: info,
+        success: function(rest) {
+            window.location.replace("//TODO: add here url");
+        }
+    });
+}
+
+function submit(){
+    $("#submit").click(() =>{
+        const info = {
+            id: $("#idinput").val(),
+            name: $("#nameinput").val(),
+            email:$("#exampleInputEmail1").val(),
+            password: $("#exampleInputPassword1").val(),
+            Categoriestogive: $(".dayav").val(),
+
+        }
+    })
+}
+
+
